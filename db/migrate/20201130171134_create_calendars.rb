3f796616d7aa52238860fd111e941bb90eeffb4a
mmtt875 <875morihana@gmail.com>
2020-12-13 12:52:19 +0900

    マイグレーションファイル変更

--- db/migrate/20201130171134_create_calendars.rb
+++ db/migrate/20201130171134_create_calendars.rb
@@ -1,8 +1,8 @@
 class CreateCalendars < ActiveRecord::Migration[6.0]
   def change
     create_table :calendars do |t|
-      t.string :name
-      t.datetime :start_time
+      t.string :title
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
